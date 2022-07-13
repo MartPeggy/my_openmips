4749d05d7edc3696b0df5c9f5693412403708d01
MartPeggy <martpeggy@tom.com>
2022-07-13 09:53:08 +0800

    fix some defines

--- defines.v
+++ defines.v
@@ -1,40 +1,40 @@
 //************************************************
 //* @FilePath     : \my_OpenMIPS\defines.v
 //* @Date         : 2022-04-24 09:53:09
-//* @LastEditTime : 2022-07-12 15:32:42
+//* @LastEditTime : 2022-07-13 09:46:55
 //* @Author       : mart
 //* @Tips         : CA+I 头注释 CA+P TB Ctrl+t 转到定义
 //* @Description  : 一些宏定义
 //************************************************
 
 //*  全局宏定义  *//
-`define RstEnable       1'b1            // 复位有效
-`define RstDisable      1'b0            // 复位无效
-`define ZeroWord        32'h00000000    // 32位的数值0
-`define WriteEnable     1'b1            // 写使能
-`define WriteDisable    1'b0            // 禁止写
-`define ReadEnable      1'b1            // 读使能
-`define ReadDisable     1'b0            // 禁止读
+`define RstEnable           1'b1            // 复位有效
+`define RstDisable          1'b0            // 复位无效
+`define ZeroWord            32'h00000000    // 32位的数值0
+`define WriteEnable         1'b1            // 写使能
+`define WriteDisable        1'b0            // 禁止写
+`define ReadEnable          1'b1            // 读使能
+`define ReadDisable         1'b0            // 禁止读
 
-`define AluOpBus        7:0             // 译码阶段的输出 aluop_o 的宽度
-`define AluSelBus       2:0             // 译码阶段的输出 alusel_o的宽度
-`define InstValid       1'b0            // 指令有效
-`define InstInvalid     1'b1            // 指令无效
-`define True_v          1'b1            // 逻辑真
-`define False_v         1'b0            // 逻辑假
-`define ChipEnable      1'b1            // 芯片使能
-`define ChipDisable     1'b0            // 芯片禁止
+`define AluOpBus            7:0             // 译码阶段的输出 aluop_o 的宽度
+`define AluSelBus           2:0             // 译码阶段的输出 alusel_o的宽度
+`define InstValid           1'b0            // 指令有效
+`define InstInvalid         1'b1            // 指令无效
+`define True_v              1'b1            // 逻辑真
+`define False_v             1'b0            // 逻辑假
+`define ChipEnable          1'b1            // 芯片使能
+`define ChipDisable         1'b0            // 芯片禁止
 
-`define Stop            1'b1            // 流水线暂停
-`define NoStop          1'b0            // 流水线继续
-`define InDelaySlot     1'b1
-`define NotInDelaySlot  1'b0
-`define Branch          1'b1
-`define NotBranch       1'b0
-`define InterruptAssert 1'b1
-`define InterruptNotAssert 1'b0
-`define TrapAssert      1'b1
-`define TrapNotAssert   1'b0 
+`define Stop                1'b1            // 流水线暂停
+`define NoStop              1'b0            // 流水线继续
+`define InDelaySlot         1'b1            // 
+`define NotInDelaySlot      1'b0            // 
+`define Branch              1'b1            // 
+`define NotBranch           1'b0            // 
+`define InterruptAssert     1'b1            //  
+`define InterruptNotAssert  1'b0         // 
+`define TrapAssert          1'b1            // 
+`define TrapNotAssert       1'b0            // 
 //*   指令宏定义    *//
 
 `define EXE_AND             6'b100100       // AND
@@ -95,86 +95,86 @@
 `define EXE_SPECIAL_INST    6'b000000       // 特殊指令
 
 //Aluop
-`define EXE_AND_OP       8'b00100100
-`define EXE_OR_OP        8'b00100101
-`define EXE_XOR_OP       8'b00100110
-`define EXE_NOR_OP       8'b00100111
-`define EXE_ANDI_OP      8'b01011001
-`define EXE_ORI_OP       8'b01011010
-`define EXE_XORI_OP      8'b01011011
-`define EXE_LUI_OP       8'b01011100
-`define EXE_SLL_OP       8'b01111100
-`define EXE_SLLV_OP      8'b00000100
-`define EXE_SRL_OP       8'b00000010
-`define EXE_SRLV_OP      8'b00000110
-`define EXE_SRA_OP       8'b00000011
-`define EXE_SRAV_OP      8'b00000111
+`define EXE_AND_OP          8'b00100100
+`define EXE_OR_OP           8'b00100101
+`define EXE_XOR_OP          8'b00100110
+`define EXE_NOR_OP          8'b00100111
+`define EXE_ANDI_OP         8'b01011001
+`define EXE_ORI_OP          8'b01011010
+`define EXE_XORI_OP         8'b01011011
+`define EXE_LUI_OP          8'b01011100
+`define EXE_SLL_OP          8'b01111100
+`define EXE_SLLV_OP         8'b00000100
+`define EXE_SRL_OP          8'b00000010
+`define EXE_SRLV_OP         8'b00000110
+`define EXE_SRA_OP          8'b00000011
+`define EXE_SRAV_OP         8'b00000111
 
-`define EXE_MOVZ_OP  8'b00001010
-`define EXE_MOVN_OP  8'b00001011
-`define EXE_MFHI_OP  8'b00010000
-`define EXE_MTHI_OP  8'b00010001
-`define EXE_MFLO_OP  8'b00010010
-`define EXE_MTLO_OP  8'b00010011
+`define EXE_MOVZ_OP         8'b00001010
+`define EXE_MOVN_OP         8'b00001011
+`define EXE_MFHI_OP         8'b00010000
+`define EXE_MTHI_OP         8'b00010001
+`define EXE_MFLO_OP         8'b00010010
+`define EXE_MTLO_OP         8'b00010011
 
-`define EXE_SLT_OP  8'b00101010
-`define EXE_SLTU_OP  8'b00101011
-`define EXE_SLTI_OP  8'b01010111
-`define EXE_SLTIU_OP  8'b01011000
-`define EXE_ADD_OP  8'b00100000
-`define EXE_ADDU_OP  8'b00100001
-`define EXE_SUB_OP  8'b00100010
-`define EXE_SUBU_OP  8'b00100011
-`define EXE_ADDI_OP  8'b01010101
-`define EXE_ADDIU_OP  8'b01010110
-`define EXE_CLZ_OP  8'b10110000
-`define EXE_CLO_OP  8'b10110001
+`define EXE_SLT_OP          8'b00101010
+`define EXE_SLTU_OP         8'b00101011
+`define EXE_SLTI_OP         8'b01010111
+`define EXE_SLTIU_OP        8'b01011000
+`define EXE_ADD_OP          8'b00100000
+`define EXE_ADDU_OP         8'b00100001
+`define EXE_SUB_OP          8'b00100010
+`define EXE_SUBU_OP         8'b00100011
+`define EXE_ADDI_OP         8'b01010101
+`define EXE_ADDIU_OP        8'b01010110
+`define EXE_CLZ_OP          8'b10110000
+`define EXE_CLO_OP          8'b10110001
 
-`define EXE_MULT_OP     8'b00011000
-`define EXE_MULTU_OP    8'b00011001
-`define EXE_MUL_OP      8'b10101001
+`define EXE_MULT_OP         8'b00011000
+`define EXE_MULTU_OP        8'b00011001
+`define EXE_MUL_OP          8'b10101001
 
-`define EXE_MADD_OP     8'b10100110
-`define EXE_MADDU_OP    8'b10101000
-`define EXE_MSUB_OP     8'b10101010
-`define EXE_MSUBU_OP    8'b10101011
+`define EXE_MADD_OP         8'b10100110
+`define EXE_MADDU_OP        8'b10101000
+`define EXE_MSUB_OP         8'b10101010
+`define EXE_MSUBU_OP        8'b10101011
 
-`define EXE_DIV_OP      8'b00011010
-`define EXE_DIVU_OP     8'b00011011
+`define EXE_DIV_OP          8'b00011010
+`define EXE_DIVU_OP         8'b00011011
 
-`define EXE_NOP_OP    8'b00000000
+`define EXE_NOP_OP          8'b00000000
 
 //AluSel
-`define EXE_RES_LOGIC 3'b001
-`define EXE_RES_SHIFT 3'b010
-`define EXE_RES_MOVE 3'b011
-`define EXE_RES_ARITHMETIC 3'b100
-`define EXE_RES_MUL 3'b101
-`define EXE_RES_NOP 3'b000
+`define EXE_RES_LOGIC       3'b001
+`define EXE_RES_SHIFT       3'b010
+`define EXE_RES_MOVE        3'b011
+`define EXE_RES_ARITHMETIC  3'b100
+`define EXE_RES_MUL         3'b101
+`define EXE_RES_NOP         3'b000
 
 
 //*   与ROM相关的宏指令    *//
-`define InstAddrBus      31:0    // ROM的地址总线宽度
-`define InstBus          31:0    // ROM的数据总线宽度
-`define InstMemNum       131071  // ROM的实际大小
-`define InstMemNumLog2   17      // ROM实际使用的地址线宽度
+`define InstAddrBus         31:0    // ROM的地址总线宽度
+`define InstBus             31:0    // ROM的数据总线宽度
+`define InstMemNum          131071  // ROM的实际大小
+`define InstMemNumLog2      17      // ROM实际使用的地址线宽度
 
 //*    与通用寄存器regfile有关的宏指令    *//
-`define RegAddrBus       4:0     // RegFile的地址线宽度
-`define RegBus           31:0    // RegFile的数据线宽度
-`define RegWidth         32      // 通用寄存器的宽度
-`define DoubleRegWidth   64      // 通用寄存器的两倍宽度
-`define DoubleRegBus     63:0    // 通用寄存器的数据线宽度的两倍
-`define RegNum           32      // 通用寄存器的数量
-`define RegNumLog2       5       // 寻址通用寄存器使用的地址位宽
-`define NOPRegAddr       5'b000000
+`define RegAddrBus          4:0     // RegFile的地址线宽度
+`define RegBus              31:0    // RegFile的数据线宽度
+`define RegWidth            32      // 通用寄存器的宽度
+`define DoubleRegWidth      64      // 通用寄存器的两倍宽度
+`define DoubleRegBus        63:0    // 通用寄存器的数据线宽度的两倍
+`define RegNum              32      // 通用寄存器的数量
+`define RegNumLog2          5       // 寻址通用寄存器使用的地址位宽
+`define NOPRegAddr          5'b000000
 
 //*     除法相关     *//
-`define DivFree         2'b00
-`define DivByZero       2'b01
-`define DivOn           2'b10
-`define DivEnd          2'b11
-`define DivResultReady  1'b1
-`define DivResultNotReady 1'b0
-`define DivStart        1'b1
-`define DivStop         1'b0
+`define DivFree             2'b00   // 
+`define DivByZero           2'b01   // 
+`define DivOn               2'b10   // 
+`define DivEnd              2'b11   // 
+`define DivResultReady      1'b1    // 
+`define DivResultNotReady   1'b0  // 
+`define DivStart            1'b1    // 
+`define DivStop             1'b0    // 
---
+++ regeneration_ctags.cmd
@@ -0,0 +1,3 @@
+cd D:\Code\my_OpenMIPS
+D:
+ctags --exclude=.vscode --languages=verilog --fields=+nKz -R -f .tags
