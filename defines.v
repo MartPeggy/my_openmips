6120d4ebd6210bd88fa09623266c8130134c77f8
MartPeggy <martpeggy@tom.com>
2022-07-21 17:25:41 +0800

    add some defines

--- defines.v
+++ defines.v
@@ -1,7 +1,7 @@
 //************************************************
 //* @FilePath     : \my_OpenMIPS\defines.v
 //* @Date         : 2022-04-24 09:53:09
-//* @LastEditTime : 2022-07-21 10:03:55
+//* @LastEditTime : 2022-07-21 17:23:29
 //* @Author       : mart
 //* @Tips         : CA+I 头注释 CA+P TB Ctrl+t 转到定义
 //* @Description  : 宏定义
@@ -90,28 +90,28 @@
 `define EXE_JALR            6'b001001       //
 `define EXE_JR              6'b001000       // 按寄存器内容转移指令
 `define EXE_BEQ             6'b000100       // 相等则转移指令
-`define EXE_BGEZ            5'b00001
-`define EXE_BGEZAL          5'b10001
-`define EXE_BGTZ            6'b000111
-`define EXE_BLEZ            6'b000110
-`define EXE_BLTZ            5'b00000
-`define EXE_BLTZAL          5'b10000
+`define EXE_BGEZ            5'b00001        //
+`define EXE_BGEZAL          5'b10001        //
+`define EXE_BGTZ            6'b000111       //
+`define EXE_BLEZ            6'b000110       //
+`define EXE_BLTZ            5'b00000        //
+`define EXE_BLTZAL          5'b10000        //
 `define EXE_BNE             6'b000101       // 不相等则转移指令
 
-`define EXE_LB              6'b100000
-`define EXE_LBU             6'b100100
-`define EXE_LH              6'b100001
-`define EXE_LHU             6'b100101
-`define EXE_LL              6'b110000
-`define EXE_LW              6'b100011
-`define EXE_LWL             6'b100010
-`define EXE_LWR             6'b100110
-`define EXE_SB              6'b101000
-`define EXE_SC              6'b111000
-`define EXE_SH              6'b101001
-`define EXE_SW              6'b101011
-`define EXE_SWL             6'b101010
-`define EXE_SWR             6'b101110
+`define EXE_LB              6'b100000       //
+`define EXE_LBU             6'b100100       //
+`define EXE_LH              6'b100001       //
+`define EXE_LHU             6'b100101       //
+`define EXE_LL              6'b110000       //
+`define EXE_LW              6'b100011       //
+`define EXE_LWL             6'b100010       //
+`define EXE_LWR             6'b100110       //
+`define EXE_SB              6'b101000       //
+`define EXE_SC              6'b111000       //
+`define EXE_SH              6'b101001       //
+`define EXE_SW              6'b101011       //
+`define EXE_SWL             6'b101010       //
+`define EXE_SWR             6'b101110       //
 
 `define EXE_SYNC            6'b001111       // SYNC 这里相当于空指令
 `define EXE_PREF            6'b110011       // PREF 这里相当于空指令
@@ -123,94 +123,94 @@
 `define EXE_SPECIAL_INST    6'b000000       // 特殊指令
 
 //*** Aluop宏定义（运算子类型） ***//
-`define EXE_AND_OP          8'b00100100
-`define EXE_OR_OP           8'b00100101
-`define EXE_XOR_OP          8'b00100110
-`define EXE_NOR_OP          8'b00100111
-`define EXE_ANDI_OP         8'b01011001
-`define EXE_ORI_OP          8'b01011010
-`define EXE_XORI_OP         8'b01011011
-`define EXE_LUI_OP          8'b01011100
-`define EXE_SLL_OP          8'b01111100
-`define EXE_SLLV_OP         8'b00000100
-`define EXE_SRL_OP          8'b00000010
-`define EXE_SRLV_OP         8'b00000110
-`define EXE_SRA_OP          8'b00000011
-`define EXE_SRAV_OP         8'b00000111
+`define EXE_AND_OP          8'b00100100     //
+`define EXE_OR_OP           8'b00100101     //
+`define EXE_XOR_OP          8'b00100110     //
+`define EXE_NOR_OP          8'b00100111     //
+`define EXE_ANDI_OP         8'b01011001     //
+`define EXE_ORI_OP          8'b01011010     //
+`define EXE_XORI_OP         8'b01011011     //
+`define EXE_LUI_OP          8'b01011100     //
+`define EXE_SLL_OP          8'b01111100     //
+`define EXE_SLLV_OP         8'b00000100     //
+`define EXE_SRL_OP          8'b00000010     //
+`define EXE_SRLV_OP         8'b00000110     //
+`define EXE_SRA_OP          8'b00000011     //
+`define EXE_SRAV_OP         8'b00000111     //
 
-`define EXE_MOVZ_OP         8'b00001010
-`define EXE_MOVN_OP         8'b00001011
-`define EXE_MFHI_OP         8'b00010000
-`define EXE_MTHI_OP         8'b00010001
-`define EXE_MFLO_OP         8'b00010010
-`define EXE_MTLO_OP         8'b00010011
+`define EXE_MOVZ_OP         8'b00001010     //
+`define EXE_MOVN_OP         8'b00001011     //
+`define EXE_MFHI_OP         8'b00010000     //
+`define EXE_MTHI_OP         8'b00010001     //
+`define EXE_MFLO_OP         8'b00010010     //
+`define EXE_MTLO_OP         8'b00010011     //
 
-`define EXE_SLT_OP          8'b00101010
-`define EXE_SLTU_OP         8'b00101011
-`define EXE_SLTI_OP         8'b01010111
-`define EXE_SLTIU_OP        8'b01011000
-`define EXE_ADD_OP          8'b00100000
-`define EXE_ADDU_OP         8'b00100001
-`define EXE_SUB_OP          8'b00100010
-`define EXE_SUBU_OP         8'b00100011
-`define EXE_ADDI_OP         8'b01010101
-`define EXE_ADDIU_OP        8'b01010110
-`define EXE_CLZ_OP          8'b10110000
-`define EXE_CLO_OP          8'b10110001
+`define EXE_SLT_OP          8'b00101010     //
+`define EXE_SLTU_OP         8'b00101011     //
+`define EXE_SLTI_OP         8'b01010111     //
+`define EXE_SLTIU_OP        8'b01011000     //
+`define EXE_ADD_OP          8'b00100000     //
+`define EXE_ADDU_OP         8'b00100001     //
+`define EXE_SUB_OP          8'b00100010     //
+`define EXE_SUBU_OP         8'b00100011     //
+`define EXE_ADDI_OP         8'b01010101     //
+`define EXE_ADDIU_OP        8'b01010110     //
+`define EXE_CLZ_OP          8'b10110000     //
+`define EXE_CLO_OP          8'b10110001     //
 
-`define EXE_MULT_OP         8'b00011000
-`define EXE_MULTU_OP        8'b00011001
-`define EXE_MUL_OP          8'b10101001
+`define EXE_MULT_OP         8'b00011000     //
+`define EXE_MULTU_OP        8'b00011001     //
+`define EXE_MUL_OP          8'b10101001     //
 
-`define EXE_MADD_OP         8'b10100110
-`define EXE_MADDU_OP        8'b10101000
-`define EXE_MSUB_OP         8'b10101010
-`define EXE_MSUBU_OP        8'b10101011
+`define EXE_MADD_OP         8'b10100110     //
+`define EXE_MADDU_OP        8'b10101000     //
+`define EXE_MSUB_OP         8'b10101010     //
+`define EXE_MSUBU_OP        8'b10101011     //
 
-`define EXE_DIV_OP          8'b00011010
-`define EXE_DIVU_OP         8'b00011011
+`define EXE_DIV_OP          8'b00011010     //
+`define EXE_DIVU_OP         8'b00011011     //
 
-`define EXE_J_OP            8'b01001111
-`define EXE_JAL_OP          8'b01010000
-`define EXE_JALR_OP         8'b00001001
-`define EXE_JR_OP           8'b00001000
-`define EXE_BEQ_OP          8'b01010001
-`define EXE_BGEZ_OP         8'b01000001
-`define EXE_BGEZAL_OP       8'b01001011
-`define EXE_BGTZ_OP         8'b01010100
-`define EXE_BLEZ_OP         8'b01010011
-`define EXE_BLTZ_OP         8'b01000000
-`define EXE_BLTZAL_OP       8'b01001010
-`define EXE_BNE_OP          8'b01010010
+`define EXE_J_OP            8'b01001111     //
+`define EXE_JAL_OP          8'b01010000     //
+`define EXE_JALR_OP         8'b00001001     //
+`define EXE_JR_OP           8'b00001000     //
+`define EXE_BEQ_OP          8'b01010001     //
+`define EXE_BGEZ_OP         8'b01000001     //
+`define EXE_BGEZAL_OP       8'b01001011     //
+`define EXE_BGTZ_OP         8'b01010100     //
+`define EXE_BLEZ_OP         8'b01010011     //
+`define EXE_BLTZ_OP         8'b01000000     //
+`define EXE_BLTZAL_OP       8'b01001010     //
+`define EXE_BNE_OP          8'b01010010     //
 
-`define EXE_LB_OP           8'b11100000
-`define EXE_LBU_OP          8'b11100100
-`define EXE_LH_OP           8'b11100001
-`define EXE_LHU_OP          8'b11100101
-`define EXE_LL_OP           8'b11110000
-`define EXE_LW_OP           8'b11100011
-`define EXE_LWL_OP          8'b11100010
-`define EXE_LWR_OP          8'b11100110
-`define EXE_PREF_OP         8'b11110011
-`define EXE_SB_OP           8'b11101000
-`define EXE_SC_OP           8'b11111000
-`define EXE_SH_OP           8'b11101001
-`define EXE_SW_OP           8'b11101011
-`define EXE_SWL_OP          8'b11101010
-`define EXE_SWR_OP          8'b11101110
-`define EXE_SYNC_OP         8'b00001111
+`define EXE_LB_OP           8'b11100000     //
+`define EXE_LBU_OP          8'b11100100     //
+`define EXE_LH_OP           8'b11100001     //
+`define EXE_LHU_OP          8'b11100101     //
+`define EXE_LL_OP           8'b11110000     //
+`define EXE_LW_OP           8'b11100011     //
+`define EXE_LWL_OP          8'b11100010     //
+`define EXE_LWR_OP          8'b11100110     //
+`define EXE_PREF_OP         8'b11110011     //
+`define EXE_SB_OP           8'b11101000     //
+`define EXE_SC_OP           8'b11111000     //
+`define EXE_SH_OP           8'b11101001     //
+`define EXE_SW_OP           8'b11101011     //
+`define EXE_SWL_OP          8'b11101010     //
+`define EXE_SWR_OP          8'b11101110     //
+`define EXE_SYNC_OP         8'b00001111     //
 
-`define EXE_NOP_OP          8'b00000000
+`define EXE_NOP_OP          8'b00000000     //
 
 //*** AluSel宏定义（运算类型） ***//
-`define EXE_RES_LOGIC       3'b001
-`define EXE_RES_SHIFT       3'b010
-`define EXE_RES_MOVE        3'b011
-`define EXE_RES_ARITHMETIC  3'b100
-`define EXE_RES_MUL         3'b101
-`define EXE_RES_JUMP_BRANCH 3'b110
-`define EXE_RES_LOAD_STORE  3'b111
-`define EXE_RES_NOP         3'b000
+`define EXE_RES_LOGIC       3'b001          //
+`define EXE_RES_SHIFT       3'b010          //
+`define EXE_RES_MOVE        3'b011          //
+`define EXE_RES_ARITHMETIC  3'b100          //
+`define EXE_RES_MUL         3'b101          //
+`define EXE_RES_JUMP_BRANCH 3'b110          //
+`define EXE_RES_LOAD_STORE  3'b111          //
+`define EXE_RES_NOP         3'b000          //
 
 //*** ROM相关的宏定义 ***//
 `define InstAddrBus         31:0            // ROM的地址总线宽度
